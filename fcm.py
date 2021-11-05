7a475a2fc559ec3dc913aeaa82032f2f42513de2
joaorelva <45034257+joaorelva@users.noreply.github.com>
2021-11-05 22:58:39 +0000

    Generator done

--- fcm.py
+++ fcm.py
@@ -10,19 +10,6 @@ def createContext(k,filename):
     textFile = open(filename, 'r')
     text = textFile.read()
 
-    """
-    arr = ""
-    count = 0
-
-    for char in text:
-        if char not in arr:
-            arr += char
-            count += 1
-
-    for c in arr:
-        print(ord(c))
-    """
-
     symbols = ['\n', ' ', '!', '"', '$', '%', "'", '(', ')', '*', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     while not ((index+k) >= len(text)) :
@@ -47,32 +34,6 @@ def createContext(k,filename):
                 table[key].update({sindex:0})
 
     return table
-
-        
-
-"""
-    while not ((index+k) >= len(text)) :
-        i = 0
-        aux = ""
-        while i < k:
-            aux += text[index]
-            index += 1
-            i += 1
-        if aux not in table.keys():
-            table.update({aux:{text[index]:1}})
-        else:
-            if text[index] in table[aux].keys():
-                table[aux][text[index]] += 1
-            else:
-                table[aux].update({text[index]:1})
-
-    for key in table.keys():
-        for sindex in symbols:
-            if sindex not in table[key].keys():
-                table[key].update({sindex:0})
-    """
-
-    #return table
 
 def calculateProbabilities(table,alpha):
     prob_table = {}
@@ -109,16 +70,59 @@ def calculateEntropy(table,prob_table,sum_total):
 #######################MAIN############################
 start = datetime.now()
 
-k = 2
-alpha = 1
+k = 3
+alpha = 0.1
 table = createContext(k,'example.txt')
-#print(table)
 prob_table,sum_total = calculateProbabilities(table,alpha)
 entropy = calculateEntropy(table,prob_table,sum_total)
-
+print("--------------------------------------------------------------")
+print("-------------------------FCM RESULTS--------------------------")
+print("--------------------------------------------------------------")
 print("For k = " + str(k) + " and alpha = " + str(alpha))
 print("Entropy of the model: " + str(entropy))
-print('Execution Time: ' + str(datetime.now() - start))
+#######################GENERATOR############################
+text = "thi"
+state = 'thi'
+length = 700
+
+
+while len(text) != length:
+    if state not in prob_table.keys():
+        text += " "
+        aux = random.choices(list(prob_table.keys()))
+        c = ' '.join(aux)
+        state = c
+    i = 0
+    index = 0
+    new_state = ""
+    #print("state : " + str(state))
+    letter = random.choices(population=list(prob_table[state].keys()), weights=prob_table[state].values())
+    car = ' '.join(letter)
+    text += car
+    #print("text : " + str(text))
+    while i < k-1:
+        new_state += state[index+1]
+        i += 1
+        index += 1
+    new_state += car
+    #print("newstate : " + str(new_state))
+    state = new_state
+
+print("--------------------------------------------------------------")
+print("------------------------TEXT GENERATED------------------------")
+print("--------------------------------------------------------------")
+
+print(text)
+
+print("--------------------------------------------------------------")
+print("------------------------EXECUTION TIME------------------------")
+print("--------------------------------------------------------------")
+print(str(datetime.now() - start))
+
+
+
+
+
 
 # k = 3 | 0.1 -> 2.6
 # k = 2 | 1   -> 2
